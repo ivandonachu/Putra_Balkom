468321b23a2226654598c449120dd1086531c4ca
ivandonachu <ivandonachu13@gmail.com>
2021-12-27 23:28:07 +0700

    new

--- CV.PBJ/Administrasi/view/DsAdministrasi.php
+++ CV.PBJ/Administrasi/view/DsAdministrasi.php
@@ -251,7 +251,7 @@ $nama = $data['nama_karyawan'];
     </div>
     <div class="col-sm-3" style="color: black; font-size: 18px;">
         <script type='text/javascript'>
-            <!--
+     
                 var months = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
                 var myDays = ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum&#39;at', 'Sabtu'];
                 var date = new Date();
@@ -262,7 +262,7 @@ $nama = $data['nama_karyawan'];
                 var yy = date.getYear();
                 var year = (yy < 1000) ? yy + 1900 : yy;
                 document.write(thisDay + ', ' + day + ' ' + months[month] + ' ' + year);
-            //-->
+   
         </script>
     </div>
 </div> 
