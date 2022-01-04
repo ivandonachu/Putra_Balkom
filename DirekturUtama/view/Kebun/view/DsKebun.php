2116a4b830f21f1c3671cb643ff7ce47689e820a
ivandonachu <ivandonachu13@gmail.com>
2022-01-04 20:55:32 +0700

    4 jan 2022

--- DirekturUtama/view/Kebun/view/DsKebun.php
+++ DirekturUtama/view/Kebun/view/DsKebun.php
@@ -1,2 +1,2 @@
 Masih Dalam Pengerjaan
-<?php echo "<a href='/DirekturUtama/view/DsDirektutUtama'><button type='button' class='btn btn-primary'>Kembali</button></a>"; ?>
+<?php echo "<a href='/DirekturUtama/view/DsDirekturUtama'><button type='button' class='btn btn-primary'>Kembali</button></a>"; ?>
