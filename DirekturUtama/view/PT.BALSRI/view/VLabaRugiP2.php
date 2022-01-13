ee87dd979fcf8d8a4fb8916ac39804f32cac081c
ivandonachu <ivandonachu13@gmail.com>
2022-01-13 11:32:02 +0700

    aa

--- DirekturUtama/view/PT.BALSRI/view/VLabaRugiP2.php
+++ DirekturUtama/view/PT.BALSRI/view/VLabaRugiP2.php
@@ -671,7 +671,7 @@ else{
 </div>
 <br>
 <br>
-<h3 class="text-center" >Laba Rugi Berdasarkan Kendaraan Palembang</h3>
+<h3 class="text-center" >Laba Rugi Berdasarkan Kendaraan Palembang </h3>
 <table id="example" class="table-sm table-striped table-bordered dt-responsive nowrap" style="width:100%; ">
 <thead>
     <tr>
