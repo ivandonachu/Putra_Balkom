d40d8372f89511db086912c56360c22694425225
ivandonachu <ivandonachu13@gmail.com>
2022-01-26 11:58:23 +0700

    a

--- DirekturUtama/view/PERTASHOP/view/VGrafikPenjualan.php
+++ DirekturUtama/view/PERTASHOP/view/VGrafikPenjualan.php
@@ -513,7 +513,7 @@ Highcharts.chart('chart_pendapatan_dexlite', {
     tooltip: {
         headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
         pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>Rp {point.y:}</b></td></tr>',
+            '<td style="padding:0"><b>Rp {point.y:.2f}</b></td></tr>',
         footerFormat: '</table>',
         shared: true,
         useHTML: true
