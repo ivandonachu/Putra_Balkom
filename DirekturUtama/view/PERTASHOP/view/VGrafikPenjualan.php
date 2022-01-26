2e6c79c585577a04c570f0ae86c16dd81e2fab1f
ivandonachu <ivandonachu13@gmail.com>
2022-01-26 11:57:37 +0700

    a

--- DirekturUtama/view/PERTASHOP/view/VGrafikPenjualan.php
+++ DirekturUtama/view/PERTASHOP/view/VGrafikPenjualan.php
@@ -572,7 +572,7 @@ Highcharts.chart('chart_penjualan_pertamax', {
     tooltip: {
         headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
         pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:}/L</b></td></tr>',
+            '<td style="padding:0"><b>{point.y:.0f}/L</b></td></tr>',
         footerFormat: '</table>',
         shared: true,
         useHTML: true
@@ -644,7 +644,7 @@ Highcharts.chart('chart_penjualan_dexlite', {
     tooltip: {
         headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
         pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.2f}/L</b></td></tr>',
+            '<td style="padding:0"><b>{point.y:.0f}/L</b></td></tr>',
         footerFormat: '</table>',
         shared: true,
         useHTML: true
