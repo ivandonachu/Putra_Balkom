99446712ed8823b75abcf933d0c02876c446b794
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-04-05 10:16:17 +0700

    5 april 2022

--- PT.BALSRI/Administrasi/view/VRincianRitDriverP.php
+++ PT.BALSRI/Administrasi/view/VRincianRitDriverP.php
@@ -276,7 +276,7 @@ else{
 
    <div class="pinggir1" style="margin-right: 20px; margin-left: 20px;">
  <div align="left">
-      <?php echo "<a href='VRitase2?tanggal1=$tanggal_awal&tanggal2=$tanggal_akhir'><button type='button' class='btn btn-primary'>Kembali</button></a>"; ?>
+      <?php echo "<a href='VRitaseP2?tanggal1=$tanggal_awal&tanggal2=$tanggal_akhir'><button type='button' class='btn btn-primary'>Kembali</button></a>"; ?>
     </div>
     <br>
     <br>
