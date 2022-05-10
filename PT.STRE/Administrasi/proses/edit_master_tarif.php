0cc808e8937a94853fec25eb2178bc34cc9bb350
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-05-11 00:09:56 +0700

    aaa

---
+++ PT.STRE/Administrasi/proses/edit_master_tarif.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+include'koneksi.php';
+if(!isset($_SESSION["login"])){
+	header("Location: logout.php");
+	exit;
+}
+$id=$_COOKIE['id_cookie'];
+$result1 = mysqli_query($koneksi, "SELECT * FROM account WHERE id_karyawan = '$id'");
+$data1 = mysqli_fetch_array($result1);
+$id1 = $data1['id_karyawan'];
+$jabatan_valid = $data1['jabatan'];
+if ($jabatan_valid == 'Administrasi') {
+
+}
+
+else{  header("Location: logout.php");
+exit;
+}
+
+
+$supply_point = $_POST['supply_point'];
+$delivery_point = $_POST['delivery_point'];
+$kf = $_POST['kf'];
+$sk_pola = $_POST['sk_pola'];
+$jt = $_POST['jt'];
+$hrg_bbm = $_POST['hrg_bbm'];
+$kl1 = $_POST['kl1'];
+$kl2 = $_POST['kl2'];
+$kl3 = $_POST['kl3'];
+
+
+
+
+
+	
+
+		$query = mysqli_query($koneksi,"UPDATE master_tarif SET supply_point = '$supply_point' , kf = '$kf' , sk_pola = '$sk_pola' , jt = '$jt' , hrg_bbm = '$hrg_bbm' , kl1 = '$kl1' , kl2 = '$kl2' , kl3 = '$kl3'  WHERE delivery_point = '$delivery_point'");
+	
+	
+			    echo "<script>alert('Update Data Berhasil :)'); window.location='../view/VMasterTarif';</script>";exit;
+
+
+
+  ?>
---
+++ PT.STRE/Administrasi/proses/proses_master_tarif.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+include'koneksi.php';
+if(!isset($_SESSION["login"])){
+	header("Location: logout.php");
+	exit;
+}
+$id=$_COOKIE['id_cookie'];
+$result1 = mysqli_query($koneksi, "SELECT * FROM account WHERE id_karyawan = '$id'");
+$data1 = mysqli_fetch_array($result1);
+$id1 = $data1['id_karyawan'];
+$jabatan_valid = $data1['jabatan'];
+if ($jabatan_valid == 'Administrasi') {
+
+}
+
+else{  header("Location: logout.php");
+exit;
+}
+
+$supply_point = $_POST['supply_point'];
+$delivery_point = $_POST['delivery_point'];
+$kf = $_POST['kf'];
+$sk_pola = $_POST['sk_pola'];
+$jt = $_POST['jt'];
+$hrg_bbm = $_POST['hrg_bbm'];
+$kl1 = $_POST['kl1'];
+$kl2 = $_POST['kl2'];
+$kl3 = $_POST['kl3'];
+
+
+
+
+
+	$query = mysqli_query($koneksi,"INSERT INTO master_tarif VALUES('$supply_point','$delivery_point','$jt','$kf','$hrg_bbm','$sk_pola','$kl1','$kl2','$kl3')");
+
+			if ($query != "") {
+			echo "<script>alert('Data Proses Berhasil :)'); window.location='../view/VMasterTarif';</script>";exit;
+
+}
+
+  ?>
