2bd5eb0eebd92e8e450451ca8ff2646e7f5c2a85
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-05-19 15:19:32 +0700

    19 mei 2022

--- PT.BALSRI/Manager/view/VRincianBackUp/VRPerbaikan.php
+++ PT.BALSRI/Manager/view/VRincianBackUp/VRPerbaikan.php
@@ -347,18 +347,19 @@ else{
 </div>
 
 
-<!-- Tabel -->    
 <table id="example" class="table-sm table-striped table-bordered dt-responsive nowrap" style="width:100%; ">
   <thead>
     <tr>
       <th>No</th>
       <th>Tanggal</th>
+      <th>Akun</th>
       <th>Nama Driver</th>
       <th>No Polisi</th>
       <th>Jumlah Pengeluaran</th>
       <th>Total</th>
       <th>Keterangan</th>
       <th>File</th>
+      <th>Aksi</th>
     </tr>
   </thead>
   <tbody>
@@ -375,6 +376,7 @@ else{
     <?php while($data = mysqli_fetch_array($table)){
       $no_laporan = $data['no_laporan'];
       $tanggal =$data['tanggal'];
+      $akun =$data['akun'];
       $nama_driver =$data['nama_driver'];
       $no_polisi =$data['no_polisi'];
       $jml_pengeluaran = $data['jml_pengeluaran'];
@@ -386,6 +388,7 @@ else{
       echo "<tr>
       <td style='font-size: 14px'>$urut</td>
       <td style='font-size: 14px'>$tanggal</td>
+      <td style='font-size: 14px'>$akun</td>
       <td style='font-size: 14px'>$nama_driver</td>
       <td style='font-size: 14px'>$no_polisi</td>
       <td style='font-size: 14px'>"?>  <?= formatuang($jml_pengeluaran); ?> <?php echo "</td>
--- PT.BALSRI/Manager/view/VRincianLRBB/VRPerbaikan.php
+++ PT.BALSRI/Manager/view/VRincianLRBB/VRPerbaikan.php
@@ -352,12 +352,14 @@ else{
     <tr>
       <th>No</th>
       <th>Tanggal</th>
+      <th>Akun</th>
       <th>Nama Driver</th>
       <th>No Polisi</th>
       <th>Jumlah Pengeluaran</th>
       <th>Total</th>
       <th>Keterangan</th>
       <th>File</th>
+      <th>Aksi</th>
     </tr>
   </thead>
   <tbody>
@@ -374,6 +376,7 @@ else{
     <?php while($data = mysqli_fetch_array($table)){
       $no_laporan = $data['no_laporan'];
       $tanggal =$data['tanggal'];
+      $akun =$data['akun'];
       $nama_driver =$data['nama_driver'];
       $no_polisi =$data['no_polisi'];
       $jml_pengeluaran = $data['jml_pengeluaran'];
@@ -385,6 +388,7 @@ else{
       echo "<tr>
       <td style='font-size: 14px'>$urut</td>
       <td style='font-size: 14px'>$tanggal</td>
+      <td style='font-size: 14px'>$akun</td>
       <td style='font-size: 14px'>$nama_driver</td>
       <td style='font-size: 14px'>$no_polisi</td>
       <td style='font-size: 14px'>"?>  <?= formatuang($jml_pengeluaran); ?> <?php echo "</td>
