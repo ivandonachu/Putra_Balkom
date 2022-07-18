aeb285fc8659e1eb37af2bb1438d07ee78cd3038
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-07-18 23:53:22 +0700

    18  juli  2022

--- index.php
+++ index.php
@@ -44,7 +44,7 @@
 
             <a href="index.html" class="logo d-flex align-items-center">
                 <img src="assets/img/melodi.png" alt="">
-                <span>Melody</span>
+                <span>Melody Group</span>
             </a>
 
             <nav id="navbar" class="navbar">
@@ -78,7 +78,7 @@
 
         <div class="container">
             <div class="copyright">
-                &copy; Copyright <strong><span> </span></strong>. All Rights Reserved
+                &copy; Copyright <strong><span>PT. Sriwijaya Teknik Energi</span></strong>. All Rights Reserved
             </div>
             <div class="credits">
                 <!-- All the links in the footer should remain intact. -->
