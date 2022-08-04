2f8019cd513e38efbd76b9cbdc8d82b8f90b0c02
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-08-04 20:51:17 +0700

    4  agustus  2022

--- PT.CBM/KasirToko/view/VRiwayatBonPembelian1.php
+++ PT.CBM/KasirToko/view/VRiwayatBonPembelian1.php
@@ -347,25 +347,7 @@ else{
              </div>
            </div>
            </div>  
-             <script>
-               $(document) .ready(function(){
 
-                var dtToday = new Date();
-
-                var month = dtToday.getMonth() +1;
-                var day = dtToday.getDate();
-                var year = dtToday.getFullYear();
-                if (month < 10)
-                  month = '0' + month.toString();
-                if (day < 10)
-                  day = '0' + day.toString();
-
-                var maxDate = year + '-' + month + '-' +day;
-
-                $('#tanggal').attr('min',maxDate);
-
-              })
-            </script>   
               
         <br>
          <div class="row">
