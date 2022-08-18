6018f2883050b264e3a124f64e4b87f99c1722da
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-08-18 18:30:23 +0700

    16  agustus  2022

--- CV.PBJ/KasirSemen/view/VPenjualan.php
+++ CV.PBJ/KasirSemen/view/VPenjualan.php
@@ -34,6 +34,9 @@ elseif (isset($_POST['tanggal1'])) {
 else{
   $tanggal_awal = date('Y-m-1');
   $tanggal_akhir = date('Y-m-31');
+
+  $bulan_sebelum = date('Y-m-d', strtotime('-3 month', strtotime($tanggal_awal))); 
+  $bulan_sesudah =  date('Y-m-d', strtotime('+1 month', strtotime($tanggal_akhir))); 
 }
 
 if ($tanggal_awal == $tanggal_akhir) {
@@ -1078,7 +1081,7 @@ $tablej2 = mysqli_query($koneksi, "SELECT no_do FROM penjualan_s WHERE tanggal_k
 
     <?php while($data = mysqli_fetch_array($tablej2)){
     $no_do_pembelian = $data['no_do'];
-    $tablexj = mysqli_query($koneksi, "SELECT no_do FROM pembelian_sl WHERE tanggal BETWEEN '$tanggal_awal' AND '$tanggal_akhir' AND no_do = '$no_do_pembelian'");
+    $tablexj = mysqli_query($koneksi, "SELECT no_do FROM pembelian_sl WHERE tanggal BETWEEN '$bulan_sebelum' AND '$bulan_sesudah' AND no_do = '$no_do_pembelian'");
 
 
     if(mysqli_num_rows($tablexj) === 0 ){
