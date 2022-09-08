9ee45825d7fdea182f4b89c4edb4c9797b86a88b
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-09-08 22:42:46 +0700

    8  september  2022

--- DirekturUtama/view/CV.PBJ/view/VLR2L.php
+++ DirekturUtama/view/CV.PBJ/view/VLR2L.php
@@ -351,16 +351,17 @@ if ($tanggal_awal == $tanggal_akhir) {
     //ety
     $tablee = mysqli_query($koneksipbj, "SELECT no_do FROM pembelian_sl WHERE tanggal BETWEEN '$tanggal_awal' AND '$tanggal_akhir'");
     $pendapatan_penjualan_ety = 0;
+    while($datae = mysqli_fetch_array($tablee)){
+        $no_do_pembelian = $datae['no_do'];
+        $tablex2 = mysqli_query($koneksipbj, "SELECT jumlah FROM penjualan_s WHERE tanggal_kirim BETWEEN '$tanggal_awal' AND '$tanggal_akhir' AND no_do = '$no_do_pembelian' AND status_bayar  = 'Lunas Transfer' OR tanggal_kirim BETWEEN '$tanggal_awal' AND '$tanggal_akhir' AND no_do = '$no_do_pembelian' AND status_bayar = 'Lunas Cash' ");
 
-        $tablex2 = mysqli_query($koneksipbj, "SELECT jumlah FROM penjualan_s WHERE tanggal_kirim BETWEEN '$tanggal_awal' AND '$tanggal_akhir' AND status_bayar  = 'Lunas Transfer' OR tanggal_kirim BETWEEN '$tanggal_awal' AND '$tanggal_akhir'  AND status_bayar = 'Lunas Cash' ");
-
-        while($datax2 = mysqli_fetch_array($tablex2)){
-          
+        if(mysqli_num_rows($tablex2) === 1 ){
+            $datax2 = mysqli_fetch_array($tablex2);
             $jumlahx = $datax2['jumlah'];
             $pendapatan_penjualan_ety = $pendapatan_penjualan_ety + $jumlahx;
               }
        
-    
+    }
 
         
     
