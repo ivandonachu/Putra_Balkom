7bd154a6f4630ca3363d0d443c109de7fa3a55ac
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-09-08 22:39:07 +0700

    8  september  2022

--- DirekturUtama/view/CV.PBJ/view/VLR2L.php
+++ DirekturUtama/view/CV.PBJ/view/VLR2L.php
@@ -355,7 +355,7 @@ if ($tanggal_awal == $tanggal_akhir) {
         $no_do_pembelian = $datae['no_do'];
         $tablex2 = mysqli_query($koneksipbj, "SELECT jumlah FROM penjualan_s WHERE tanggal_kirim BETWEEN '$tanggal_awal' AND '$tanggal_akhir' AND status_bayar  = 'Lunas Transfer' OR tanggal_kirim BETWEEN '$tanggal_awal' AND '$tanggal_akhir'  AND status_bayar = 'Lunas Cash' ");
 
-        if(mysqli_num_rows($tablex2) === 1 ){
+        while(mysqli_num_rows($tablex2)){
             $datax2 = mysqli_fetch_array($tablex2);
             $jumlahx = $datax2['jumlah'];
             $pendapatan_penjualan_ety = $pendapatan_penjualan_ety + $jumlahx;
