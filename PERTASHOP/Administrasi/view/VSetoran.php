2d86a5e55b8e40f8388a0724671cef27f2199ed1
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-09-20 09:43:33 +0700

    19  september  2022

--- PERTASHOP/Administrasi/view/VSetoran.php
+++ PERTASHOP/Administrasi/view/VSetoran.php
@@ -377,7 +377,8 @@ else{
 
 
 <!-- Tabel -->    
-<table id="example" class="table-sm table-striped table-bordered dt-responsive nowrap" style="width:100%; ">
+<div style="overflow-x: auto" align = 'center'>
+              <table id="example" class="table-sm table-striped table-bordered  nowrap" style="width:auto">
   <thead>
     <tr>
       <th>No</th>
@@ -602,6 +603,7 @@ else{
 </tbody>
 </table>
 </div>
+  </div>
 <br>
 <br>
 <div class="row" style="margin-right: 20px; margin-left: 20px;">
