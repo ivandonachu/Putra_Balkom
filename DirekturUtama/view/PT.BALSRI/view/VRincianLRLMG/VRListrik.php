0d28664c1333481d04b8611f7fc5a63752a3676c
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-09-26 21:32:33 +0700

    24  september  2022

--- DirekturUtama/view/PT.BALSRI/view/VRincianLRLMG/VRListrik.php
+++ DirekturUtama/view/PT.BALSRI/view/VRincianLRLMG/VRListrik.php
@@ -30,7 +30,7 @@ if ($tanggal_awal == $tanggal_akhir) {
   $table = mysqli_query($koneksibalsri, "SELECT * FROM pengeluaran_pul WHERE tanggal = '$tanggal_awal' AND nama_akun = 'Listrik & Telepon' ");
 }
 else{
-  $table = mysqli_query($koneksibalsri, "SELECT * FROM pengeluaran_pul WHERE tanggal BETWEEN '$tanggal_awal' AND '$tanggal_akhir' AND nama_akun = 'Listrik & Telepon'");
+  $table = mysqli_query($koneksibalsri, "SELECT * FROM pengeluaran_pul WHERE tanggal BETWEEN '$tanggal_awal' AND '$tanggal_akhir' AND nama_akun = 'Listrik & Telepon' ORDER BY tanggal ");
 }
 
 
