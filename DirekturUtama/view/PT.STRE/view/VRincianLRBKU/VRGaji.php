67ae78b0acc15122392e5d9f762e009300691ab6
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-10-13 09:54:13 +0700

    11  OKTOBER  2022

--- DirekturUtama/view/PT.STRE/view/VRincianLRBKU/VRGaji.php
+++ DirekturUtama/view/PT.STRE/view/VRincianLRBKU/VRGaji.php
@@ -22,12 +22,12 @@ $tanggal_awal = $_GET['tanggal1'];
 $tanggal_akhir = $_GET['tanggal2'];
 
 if ($tanggal_awal == $tanggal_akhir) {
-    $table = mysqli_query($koneksistre, "SELECT * FROM riwayat_penggajian WHERE tanggal  = '$tanggal_awal'  AND referensi = 'Driver LMG' OR tanggal  = '$tanggal_awal'  AND referensi = 'BALSRI LMG'");
+    $table = mysqli_query($koneksistre, "SELECT * FROM riwayat_penggajian WHERE tanggal  = '$tanggal_awal'  AND referensi = 'Driver LMG' OR tanggal  = '$tanggal_awal'  AND referensi = 'BALSRI BKU'");
   
   }
   
   else{
-    $table = mysqli_query($koneksistre, "SELECT * FROM riwayat_penggajian WHERE tanggal  BETWEEN '$tanggal_awal' AND '$tanggal_akhir' AND referensi = 'Driver LMG' OR tanggal  BETWEEN '$tanggal_awal' AND '$tanggal_akhir' AND referensi = 'BALSRI LMG'");
+    $table = mysqli_query($koneksistre, "SELECT * FROM riwayat_penggajian WHERE tanggal  BETWEEN '$tanggal_awal' AND '$tanggal_akhir' AND referensi = 'Driver LMG' OR tanggal  BETWEEN '$tanggal_awal' AND '$tanggal_akhir' AND referensi = 'BALSRI BKU'");
   
   }
 
