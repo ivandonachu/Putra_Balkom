327d45942f2af73a20d4cf93fd204065fa1cef91
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-10-13 20:45:59 +0700

    13  OKTOBER  2022

--- DirekturUtama/view/PT.BALSRI/view/VGajiP2.php
+++ DirekturUtama/view/PT.BALSRI/view/VGajiP2.php
@@ -418,7 +418,7 @@ else{
       echo "<tr>
       <td style='font-size: 14px' align = 'center'>$amt</td>
       <td style='font-size: 14px' align = 'center'>"?>  <?= formatuang($mt); ?> <?php echo "</td>
-      <td align = 'center'><a href='VRincianGaji?tanggal1=$tanggal_awal&tanggal2=$tanggal_akhir&nama_driver=$amt'>Rincian</a></td>
+      <td align = 'center'><a href='VRincianGajiP?tanggal1=$tanggal_awal&tanggal2=$tanggal_akhir&nama_driver=$amt'>Rincian</a></td>
       </tr>";
 }
 ?>
