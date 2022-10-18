dfdbedb4213822e676f12bb5f2c65d87a52edb5e
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-10-18 23:40:48 +0700

    18  Oktober v2  2022

--- DirekturUtama/view/PT.MESPBR/view/VPengeluaranWorkshop.php
+++ DirekturUtama/view/PT.MESPBR/view/VPengeluaranWorkshop.php
@@ -53,7 +53,7 @@ else{
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>Workshop CBM</title>
+  <title>Workshop MES/PBR</title>
 
   <!-- Custom fonts for this template-->
   <link href="/sbadmin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -179,7 +179,7 @@ else{
 
       <!-- Topbar -->
       <nav class="navbar navbar-expand navbar-light  topbar mb-4 static-top shadow" style="background-color:#2C7873;">
-      <?php echo "<a href='VPengeluaranWorkshop'><h5 class='text-center sm' style='color:white; margin-top: 8px;  '>Workshop CBM</h5></a>"; ?>
+      <?php echo "<a href='VPengeluaranWorkshop'><h5 class='text-center sm' style='color:white; margin-top: 8px;  '>Workshop MES/PBR</h5></a>"; ?>
 
        <!-- Sidebar Toggle (Topbar) -->
        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
