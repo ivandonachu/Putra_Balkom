c2cdf8e3317225dfcbe8067e0839b73892804344
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-10-19 11:21:40 +0700

    18  Oktober v2  2022

--- DirekturUtama/view/CV.PBJ/view/VPenebusanL.php
+++ DirekturUtama/view/CV.PBJ/view/VPenebusanL.php
@@ -1079,7 +1079,7 @@ else{
 <br>
 <?php 
 
-$tablej2 = mysqli_query($koneksipbj, "SELECT  tanggal_do, tanggal_kirim , no_do, tujuan_pengiriman, qty FROM penjualan_s WHERE tanggal_kirim BETWEEN '$tanggal_awal' AND '$bulan_sesudah'");
+$tablej2 = mysqli_query($koneksipbj, "SELECT  tanggal_do, tanggal_kirim , no_do, tujuan_pengiriman, qty FROM penjualan_s WHERE tanggal_kirim BETWEEN '$tanggal_awal' AND '$tanggal_akhir'");
 
 ?>
 
@@ -1113,7 +1113,7 @@ $tablej2 = mysqli_query($koneksipbj, "SELECT  tanggal_do, tanggal_kirim , no_do,
     $qty = $data['qty'];
 
 
-      $tablexj = mysqli_query($koneksipbj, "SELECT no_do FROM pembelian_sl WHERE tanggal BETWEEN '$tanggal_awal' AND '$tanggal_akhir' AND no_do = '$no_do_pembelian'");
+      $tablexj = mysqli_query($koneksipbj, "SELECT no_do FROM pembelian_sl WHERE tanggal BETWEEN '$belun_sebelum' AND '$tanggal_akhir' AND no_do = '$no_do_pembelian'");
 
 
       if(mysqli_num_rows($tablexj) === 0 ){
