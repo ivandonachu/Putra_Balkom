42bb6dcbf4fc28a9131dda042a87e65816827bac
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-10-22 14:54:33 +0700

    22  Oktober  2022

--- PT.CBM/StaffAdmin/view/VBonPribadi.php
+++ PT.CBM/StaffAdmin/view/VBonPribadi.php
@@ -427,7 +427,7 @@ else{
       <td style='font-size: 14px'>$jumlah_angsuran</td>
       <td style='font-size: 14px'>$status_hutang</td>
       <td style='font-size: 14px'>$keterangan</td>
-      <td style='font-size: 14px'>"; ?> <a download="../file_staf_admin/<?= $file_bukti ?>" href="../file_staf_admin/<?= $file_bukti ?>"> <?php echo "$file_bukti </a> </td>
+      <td style='font-size: 14px'>"; ?> <a download="../file_staff_admin/<?= $file_bukti ?>" href="../file_staff_admin/<?= $file_bukti ?>"> <?php echo "$file_bukti </a> </td>
       "; ?>
       <?php echo "<td style='font-size: 12px'>"; ?>
 
