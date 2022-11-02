7e797765fb0dec4e2c3a40fa313597228d54c495
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-11-02 15:39:27 +0700

    2  november  2022

--- DirekturUtama/view/CV.PBJ/view/VRincianLR/VRCashback.php
+++ DirekturUtama/view/CV.PBJ/view/VRincianLR/VRCashback.php
@@ -6,22 +6,17 @@ if(!isset($_SESSION["login"])){
   exit;
 }
 $id=$_COOKIE['id_cookie'];
-$result1 = mysqli_query($koneksi, "SELECT * FROM account WHERE id_karyawan = '$id'");
+$result1 = mysqli_query($koneksicbm, "SELECT * FROM super_account WHERE username = '$id'");
 $data1 = mysqli_fetch_array($result1);
-$id1 = $data1['id_karyawan'];
-$foto_profile = $data1['foto_profile'];
+$nama = $data1['nama_pemilik'];
 $jabatan_valid = $data1['jabatan'];
-if ($jabatan_valid == 'Admin Semen') {
+if ($jabatan_valid == 'Direktur Utama') {
 
 }
 
-else{  header("Location: logout.php");
+else{ header("Location: logout.php");
 exit;
 }
-$result = mysqli_query($koneksi, "SELECT * FROM karyawan WHERE id_karyawan = '$id1'");
-$data = mysqli_fetch_array($result);
-$nama = $data['nama_karyawan'];
-
 
 
 if (isset($_GET['tanggal1'])) {
