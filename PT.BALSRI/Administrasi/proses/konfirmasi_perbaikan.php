a94fe40c37816d9e8a278ef672564379ea68e0b2
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-11-10 12:21:26 +0700

    10  november  2022

--- PT.BALSRI/Administrasi/proses/konfirmasi_perbaikan.php
+++ PT.BALSRI/Administrasi/proses/konfirmasi_perbaikan.php
@@ -22,27 +22,43 @@ $tanggal_awal = $_POST['tanggal1'];
 $tanggal_akhir = $_POST['tanggal2'];
 $no_polisi = $_POST['no_polisi'];
 $tanggal = $_POST['tanggal'];
-$lokasi = $_POST['lokasi'];
+
+$result2 = mysqli_query($koneksi, "SELECT * FROM kendaraan WHERE no_polisi = '$no_polisi' ");
+$data_kendaraan = mysqli_fetch_array($result2);
+$lokasi = $data_kendaraan['wilayah_operasi'];
+$jenis_kendaraan = $data_kendaraan['jenis_kendaraan'];
 
 
 	
 
 		$query = mysqli_query($koneksi,"UPDATE kendaraan SET tgl_perbaikan = '$tanggal' WHERE no_polisi = '$no_polisi'");
-		$query2 = mysqli_query($koneksi,"UPDATE kendaraan SET tgl_perbaikan = '$tanggal' WHERE no_polisi = '$no_polisi'");
 
-		if($lokasi == 'Lampung'){
-			echo "<script>alert('Data Berhasil Di Edit :)'); window.location='../view/VRitase2?tanggal1=$tanggal_awal&tanggal2=$tanggal_akhir';</script>";exit;
+		if($jenis_kendaraan == '5000 L'){
+			if($lokasi == 'Lampung'){
+				echo "<script>alert('Data Berhasil Di Edit :)'); window.location='../view/VRitase2?tanggal1=$tanggal_awal&tanggal2=$tanggal_akhir';</script>";exit;
+				
+			}
+			elseif($lokasi == 'Palembang'){
+				echo "<script>alert('Data Berhasil Di Edit :)'); window.location='../view/VRitaseP2?tanggal1=$tanggal_awal&tanggal2=$tanggal_akhir';</script>";exit;
+			}
+			elseif($lokasi == 'Baturaja'){
+				echo "<script>alert('Data Berhasil Di Edit :)'); window.location='../view/VRitaseBr?tanggal1=$tanggal_awal&tanggal2=$tanggal_akhir';</script>";exit;
+			}
+			elseif($lokasi == 'Belitung'){
+				echo "<script>alert('Data Berhasil Di Edit :)'); window.location='../view/VRitaseBl?tanggal1=$tanggal_awal&tanggal2=$tanggal_akhir';</script>";exit;
+			}
+			elseif($lokasi == 'Bangka'){
+				echo "<script>alert('Data Berhasil Di Edit :)'); window.location='../view/VRitaseBk?tanggal1=$tanggal_awal&tanggal2=$tanggal_akhir';</script>";exit;
+			}
+		}
+		else{
+			if($lokasi == 'Lampung'){
+				echo "<script>alert('Data Berhasil Di Edit :)'); window.location='../view/VRitaseL8?tanggal1=$tanggal_awal&tanggal2=$tanggal_akhir';</script>";exit;
+				
+			}
 			
 		}
-		elseif($lokasi == 'Palembang'){
-			echo "<script>alert('Data Berhasil Di Edit :)'); window.location='../view/VRitaseP2?tanggal1=$tanggal_awal&tanggal2=$tanggal_akhir';</script>";exit;
-		}
-		elseif($lokasi == 'Baturaja'){
-			echo "<script>alert('Data Berhasil Di Edit :)'); window.location='../view/VRitaseBr?tanggal1=$tanggal_awal&tanggal2=$tanggal_akhir';</script>";exit;
-		}
-		elseif($lokasi == 'Belitung'){
-			echo "<script>alert('Data Berhasil Di Edit :)'); window.location='../view/VRitaseBr?tanggal1=$tanggal_awal&tanggal2=$tanggal_akhir';</script>";exit;
-		}
+		
 			
 			
 
