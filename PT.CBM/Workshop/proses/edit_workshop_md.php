f51bce1de4319582cdde291c6ab9a870b36c0bea
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2022-11-25 13:05:36 +0700

    14  november  2022

--- PT.CBM/Workshop/proses/edit_workshop_md.php
+++ PT.CBM/Workshop/proses/edit_workshop_md.php
@@ -49,7 +49,7 @@ else if ( $nama_file != "" ) {
 		$nama_file_baru .= ".";
 		$nama_file_baru .= $ekstensi_file;
 
-		move_uploaded_file($tmp_name, '../gambar/' . $nama_file_baru   );
+		move_uploaded_file($tmp_name, '../file_workshop/' . $nama_file_baru   );
 
 		return $nama_file_baru; 
 
