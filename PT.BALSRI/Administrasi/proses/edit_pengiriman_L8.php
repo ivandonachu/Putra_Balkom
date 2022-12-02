bdf08e41d6c0aa13da4077feafaa21d820fccc73
ivandonachu <ivandonachu13@gmail.com>
2022-12-02 20:29:35 +0700

    2 desember 2022

--- PT.BALSRI/Administrasi/proses/edit_pengiriman_L8.php
+++ PT.BALSRI/Administrasi/proses/edit_pengiriman_L8.php
@@ -43,7 +43,7 @@ else{
 }
 
 
-$u_dex = $dexlite*18100;
+$u_dex = $dexlite*18550;
 $uang_makan = 625*$jt_gps;
 $uang_jalan = $u_dex + $uang_makan;
 
--- PT.BALSRI/Administrasi/proses/proses_pengiriman_L8.php
+++ PT.BALSRI/Administrasi/proses/proses_pengiriman_L8.php
@@ -45,7 +45,7 @@ else{
 	$dexlite = $jt_gps/6;
 }
 
-$u_dex = $dexlite*18100;
+$u_dex = $dexlite*18550;
 $uang_makan = 625*$jt_gps;
 $uang_jalan = $u_dex + $uang_makan;
 if ($jns_trans == 'Lost') {
