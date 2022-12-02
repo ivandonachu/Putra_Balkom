37fcbccdb8e28829ce646f5711e16dc2da2ca71a
ivandonachu <ivandonachu13@gmail.com>
2022-12-02 20:04:04 +0700

    2 desember 2022

--- DirekturUtama/view/CV.PBJ/view/VPenebusanL.php
+++ DirekturUtama/view/CV.PBJ/view/VPenebusanL.php
@@ -146,6 +146,12 @@ else{
   
 
   //Curah OPC Type 1 Debit
+  $table1x = mysqli_query($koneksipbj, "SELECT SUM(jumlah) AS uang_pembelian  FROM pembelian_sl WHERE  tanggal BETWEEN 
+  '$tanggal_awal' AND '$tanggal_akhir' ");
+  $data2x = mysqli_fetch_array($table1x);
+  $total_uang_pembelian = $data2x['uang_pembelian'];
+
+  //Curah OPC Type 1 Debit
   $table2 = mysqli_query($koneksipbj, "SELECT SUM(qty) AS penjualan_copct1_debit ,  SUM(jumlah) AS uang_copct1_debit  FROM pembelian_sl WHERE  tanggal BETWEEN 
   '$tanggal_awal' AND '$tanggal_akhir' AND tipe_bayar = 'Debit' AND material = 'Curah OPC Type 1'");
   $data2 = mysqli_fetch_array($table2);
@@ -1074,6 +1080,25 @@ else{
     </div>
   </div>
 </div>
+
+<br>
+
+  <div class="col-xl-12 col-md-12 mb-12">
+    <div class="card border-left-success shadow h-100 py-2">
+      <div class="card-body">
+        <div class="row no-gutters align-items-center">
+          <div class="col mr-2">
+            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+            Uang Total Penebusan</div>
+            <div class="h5 mb-0 font-weight-bold text-gray-800"><?=  formatuang($total_uang_pembelian) ?></div>
+          </div>
+          <div class="col-auto">
+            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
 <br>
 <br>
