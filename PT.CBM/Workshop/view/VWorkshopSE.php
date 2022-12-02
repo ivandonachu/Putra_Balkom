90f9e7e178abb4b640d437c6ac5c4029a8482783
ivandonachu <ivandonachu13@gmail.com>
2022-12-02 09:58:46 +0700

    no message

--- PT.CBM/Workshop/view/VWorkshopSE.php
+++ PT.CBM/Workshop/view/VWorkshopSE.php
@@ -368,7 +368,7 @@ else{
 <div class="modal fade" id="formedit<?php echo $data['no_laporan']; ?>" role="dialog" arialabelledby="modalLabel" aria-hidden="true">
   <div class="modal-dialog" role ="document">
     <div class="modal-content"> 
-      <div class="modal-header">Form Edit Kas Kecil </h5>
+      <div class="modal-header">Form Edit Workshop Semen </h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="close">
           <span aria-hidden="true"> &times; </span>
         </button>
