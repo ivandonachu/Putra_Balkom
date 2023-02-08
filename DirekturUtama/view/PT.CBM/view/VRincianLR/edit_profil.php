3d2095a8c3b0df6cfd3a57256fac930fc89dafa5
LAPTOP-UMICODQ2\Lenovo <ivandonachu13@gmail.com>
2023-02-08 19:02:37 +0800

    8  februari  2023

--- DirekturUtama/view/PT.CBM/view/VRincianLR/edit_profil.php
+++ DirekturUtama/view/PT.CBM/view/VRincianLR/edit_profil.php
@@ -45,7 +45,7 @@ else if ( $nama_file != "" ) {
 		$nama_file_baru .= ".";
 		$nama_file_baru .= $ekstensi_file;
 
-		move_uploaded_file($tmp_name, '../../../../assets/img/foto_profile/' . $nama_file_baru   );
+		move_uploaded_file($tmp_name, '../../../../../assets/img/foto_profile/' . $nama_file_baru   );
 
 		return $nama_file_baru; 
 
