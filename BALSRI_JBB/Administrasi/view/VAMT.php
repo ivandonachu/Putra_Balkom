a8301131a272eedbf1c9a49cc7db3d3a85897a73
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2023-04-04 09:59:33 +0700

    2 april  2023

--- BALSRI_JBB/Administrasi/view/VAMT.php
+++ BALSRI_JBB/Administrasi/view/VAMT.php
@@ -227,7 +227,7 @@ $table = mysqli_query($koneksi, "SELECT * FROM driver");
 
     <!-- Topbar -->
     <nav class="navbar navbar-expand navbar-light  topbar mb-4 static-top shadow" style="background-color:#2C7873;">
-
+    <?php echo "<a href=''><h5 class='text-center sm' style='color:white; margin-top: 8px; '>List AMT</h5></a>"; ?>
         <!-- Sidebar Toggle (Topbar) -->
         <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
             <i class="fa fa-bars"></i>
