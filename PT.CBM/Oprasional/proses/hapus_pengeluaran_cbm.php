ac2ba496a4b06ce5118bbdd8fe45a4702799f480
ivandonachu <ivandonachu13@gmail.com>
2023-06-16 11:11:39 +0700

    16 Juni  2023

--- PT.CBM/Oprasional/proses/hapus_pengeluaran_cbm.php
+++ PT.CBM/Oprasional/proses/hapus_pengeluaran_cbm.php
@@ -1,16 +1,17 @@
+
 <?php
 session_start();
 include'koneksi.php';
 if(!isset($_SESSION["login"])){
-	header("Location: logout.php");
-	exit;
+  header("Location: logout.php");
+  exit;
 }
 $id=$_COOKIE['id_cookie'];
 $result1 = mysqli_query($koneksi, "SELECT * FROM account WHERE id_karyawan = '$id'");
 $data1 = mysqli_fetch_array($result1);
 $id1 = $data1['id_karyawan'];
 $jabatan_valid = $data1['jabatan'];
-if ($jabatan_valid == 'Administrasi') {
+if ($jabatan_valid == 'Kepala Oprasional') {
 
 }
 
