bb4336adee79e7269d1fb24543497e1e096b4461
ivandonachu <ivandonachu13@gmail.com>
2023-08-04 21:41:59 +0800

    4 Agusutus 2023

--- DirekturUtama/view/CV.PBJ/view/VLR2L.php
+++ DirekturUtama/view/CV.PBJ/view/VLR2L.php
@@ -543,17 +543,17 @@ else {
 
      // pembelian kadek dan etty
 
-     $tabel = mysqli_query($koneksipbj, "SELECT tanggal_do FROM penjualan_sl WHERE tanggal_kirim BETWEEN '$tanggal_awal' AND '$tanggal_akhir' ORDER BY tanggal_kirim DESC LIMIT 1 ");
+     $tabel = mysqli_query($koneksipbj, "SELECT tanggal_kirim FROM penjualan_sl WHERE tanggal_kirim BETWEEN '$tanggal_awal' AND '$tanggal_akhir' ORDER BY tanggal_kirim DESC LIMIT 1 ");
      $datal = mysqli_fetch_array($tabel);
 
-     $tanggal_do_max = $datal['tanggal_do'];
+     $tanggal_do_max = $datal['tanggal_kirim'];
 
-     $tabel2 = mysqli_query($koneksipbj, "SELECT tanggal_do FROM penjualan_sl WHERE tanggal_kirim BETWEEN '$tanggal_awal' AND '$tanggal_akhir' ORDER BY tanggal_kirim ASC LIMIT 1 ");
+     $tabel2 = mysqli_query($koneksipbj, "SELECT tanggal_kirim FROM penjualan_sl WHERE tanggal_kirim BETWEEN '$tanggal_awal' AND '$tanggal_akhir' ORDER BY tanggal_kirim ASC LIMIT 1 ");
      $data2 = mysqli_fetch_array($tabel2);
 
-     $tanggal_do_min = $data2['tanggal_do']; 
+     $tanggal_do_min = $data2['tanggal_kirim'];
 
-     var_dump($tanggal_do_min);
+
 
      $tablex = mysqli_query($koneksipbj, "SELECT SUM(jumlah) AS total_pembelian FROM pembelian_sl WHERE tanggal BETWEEN '$tanggal_do_min' AND '$tanggal_do_max' ");
      $datax = mysqli_fetch_array($tablex);
