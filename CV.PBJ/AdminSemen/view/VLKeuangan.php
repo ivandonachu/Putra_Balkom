4827212e4a8ed5abd0fe6d88c467bc0f8bdd97ff
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2023-11-09 16:15:03 +0700

    9 November 2023

--- CV.PBJ/AdminSemen/view/VLKeuangan.php
+++ CV.PBJ/AdminSemen/view/VLKeuangan.php
@@ -282,7 +282,6 @@ if ($tanggal_awal == $tanggal_akhir) {
                               <option>Bayar Sewa Kendaraan Luar</option>
                               <option>Pendapatan Sewa</option>
                               <option>Ongkos Kuli</option>
-                              <option>Ongkos Mobil</option>
                               <option>Uang Jalan</option>
                               <option>Biaya Konsumsi</option>
                               <option>Biaya Administrasi</option>
@@ -437,7 +436,6 @@ if ($tanggal_awal == $tanggal_akhir) {
                                     <option <?php echo ($dataSelect == 'Setor Uang') ? "selected" : "" ?>>Setor Uang</option>
                                     <option <?php echo ($dataSelect == 'Bayar Sewa Kendaraan Luar') ? "selected" : "" ?>>Bayar Sewa Kendaraan Luar</option>
                                     <option <?php echo ($dataSelect == 'Ongkos Kuli') ? "selected" : "" ?>>Ongkos Kuli</option>
-                                    <option <?php echo ($dataSelect == 'Ongkos Mobil') ? "selected" : "" ?>>Ongkos Mobil</option>
                                     <option <?php echo ($dataSelect == 'Uang Jalan') ? "selected" : "" ?>>Uang Jalan</option>
                                     <option <?php echo ($dataSelect == 'Biaya Konsumsi') ? "selected" : "" ?>>Biaya Konsumsi</option>
                                     <option <?php echo ($dataSelect == 'Biaya Administrasi') ? "selected" : "" ?>>Biaya Administrasi</option>
