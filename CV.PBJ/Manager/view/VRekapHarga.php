009da844672460f292df155665427d8e4c45d9e0
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2023-11-21 13:25:14 +0700

    21 November 2023

--- CV.PBJ/Manager/view/VRekapHarga.php
+++ CV.PBJ/Manager/view/VRekapHarga.php
@@ -343,7 +343,7 @@ Logout
   <div class="pinggir1" style="margin-right: 20px; margin-left: 20px;">
 
 
-    <?php  echo "<form  method='POST' action='VRekapanHarga' style='margin-bottom: 15px;'>" ?>
+    <?php  echo "<form  method='POST' action='VRekapHarga' style='margin-bottom: 15px;'>" ?>
     <div>
       <div align="left" style="margin-left: 20px;"> 
         <input type="date" id="tanggal1" style="font-size: 14px" name="tanggal1"> 
