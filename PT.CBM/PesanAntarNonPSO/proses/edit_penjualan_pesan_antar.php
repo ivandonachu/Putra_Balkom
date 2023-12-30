bcbb07963a67e0207bfb79d190989f13e91ed0c1
ivandonachu <ivandonachu13@gmail.com>
2023-12-30 08:12:31 +0700

    30  Desember 2023

--- PT.CBM/PesanAntarNonPSO/proses/edit_penjualan_pesan_antar.php
+++ PT.CBM/PesanAntarNonPSO/proses/edit_penjualan_pesan_antar.php
@@ -1,4 +1,22 @@
-c
+<?php
+session_start();
+include 'koneksi.php';
+if (!isset($_SESSION["login"])) {
+  header("Location: logout.php");
+  exit;
+}
+$id = $_COOKIE['id_cookie'];
+$result1 = mysqli_query($koneksi, "SELECT * FROM account WHERE id_karyawan = '$id'");
+$data1 = mysqli_fetch_array($result1);
+$id1 = $data1['id_karyawan'];
+$foto_profile = $data1['foto_profile'];
+$jabatan_valid = $data1['jabatan'];
+if ($jabatan_valid == 'CS Pesan Antar Non PSO') {
+} else {
+  header("Location: logout.php");
+  exit;
+}
+
 $no_transaksi = $_POST['no_transaksi'];
 $tanggal_awal = $_POST['tanggal1'];
 $tanggal_akhir = $_POST['tanggal2'];
