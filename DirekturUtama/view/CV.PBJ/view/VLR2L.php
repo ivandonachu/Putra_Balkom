f3ee8932ae2fcfe0b0d5d60130d567cca3306c63
ivandonachu <ivandonachu13@gmail.com>
2024-01-02 09:54:25 +0700

    30  Desember 2023

--- DirekturUtama/view/CV.PBJ/view/VLR2L.php
+++ DirekturUtama/view/CV.PBJ/view/VLR2L.php
@@ -94,7 +94,7 @@ if ($tanggal_awal == $tanggal_akhir) {
     }
 
     //ety
-    $tablex2 = mysqli_query($koneksipbj, "SELECT jumlah FROM penjualan_s WHERE tanggal_kirim BETWEEN '$tanggal_awal' AND '$tanggal_akhir'  AND status_bayar  = 'Lunas Transfer' OR tanggal_kirim BETWEEN '$tanggal_awal' AND '$tanggal_akhir'  AND tujuan_pengiriman != 'Gudang Mesuji'  AND status_bayar = 'Lunas Cash' AND tujuan_pengiriman != 'Gudang Mesuji' ");
+    $tablex2 = mysqli_query($koneksipbj, "SELECT jumlah FROM penjualan_s WHERE tanggal_kirim BETWEEN '$tanggal_awal' AND '$tanggal_akhir'  AND status_bayar  = 'Lunas Transfer' OR tanggal_kirim BETWEEN '$tanggal_awal' AND '$tanggal_akhir'  AND status_bayar = 'Lunas Cash'  ");
     $pendapatan_penjualan_ety = 0;
 
     while ($datae = mysqli_fetch_array($tablex2)) {
