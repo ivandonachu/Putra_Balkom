3b0b6b91696552a2db4d51d6275d2e88d4d46218
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2024-03-02 10:20:46 +0700

    1 Maret 2024

--- PT.CBM/StaffAdmin/view/VListGajiPBR.php
+++ PT.CBM/StaffAdmin/view/VListGajiPBR.php
@@ -292,7 +292,7 @@ $table = mysqli_query($koneksi, "SELECT * FROM list_gaji_pbr");
 
   <!-- Name Page -->
   <div class="pinggir1" style="margin-right: 20px; margin-left: 20px;">
-  <?php echo "<form  method='POST' action='VListGajiMES' style='margin-bottom: 15px;'>" ?>
+  <?php echo "<form  method='POST' action='VListGajiPBR' style='margin-bottom: 15px;'>" ?>
             <div>
               <div align="left" style="margin-left: 20px;">
                 <input type="date" id="tanggal1" style="font-size: 14px" name="tanggal1">
