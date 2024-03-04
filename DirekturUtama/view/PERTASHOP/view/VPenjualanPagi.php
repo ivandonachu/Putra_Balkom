14d3b817eeba35c1e3eb8f1f4df92fee76ec9e9d
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2024-03-04 22:56:16 +0700

    3 Maret 2024

--- DirekturUtama/view/PERTASHOP/view/VPenjualanPagi.php
+++ DirekturUtama/view/PERTASHOP/view/VPenjualanPagi.php
@@ -394,7 +394,7 @@ else{
   </tr>
   <tr>
       <td style='font-size: 11px' align = 'center'>Muara Dua</td>
-      <td style='font-size: 11px' align = 'center'>Dexlite</td>
+      <td style='font-size: 11px' align = 'center'>Pertamax</td>
       <td style='font-size: 11px' align = 'center'><?=  ($terjual_md); ?> Liter</td>
      
   </tr>
