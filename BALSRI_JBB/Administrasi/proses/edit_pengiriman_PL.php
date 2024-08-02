5aff88e49f5da4b337ba6120251f979961370cb5
ivandonachu <ivandonachu13@gmail.com>
2024-08-02 11:32:53 +0700

    1 agustus  2024

--- BALSRI_JBB/Administrasi/proses/edit_pengiriman_PL.php
+++ BALSRI_JBB/Administrasi/proses/edit_pengiriman_PL.php
@@ -41,7 +41,7 @@ if ($jen_ken == '8000 L') {
 	$solar = $jt_gps / 6;
 }
 
-$u_dex = $dexlite * 14550;
+$u_dex = $dexlite * 15350;
 $u_solar = $solar * 12000;
 $uang_makan = 625 * $jt_gps;
 $uang_jalan = $u_dex + $uang_makan;
