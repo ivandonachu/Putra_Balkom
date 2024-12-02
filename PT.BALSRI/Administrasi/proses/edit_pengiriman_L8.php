e7873228efdd32ec29ead5ef82f630d06c0549ba
ivandonachu <ivandonachu13@gmail.com>
2024-12-02 18:07:56 +0700

    1 November 2024

--- PT.BALSRI/Administrasi/proses/edit_pengiriman_L8.php
+++ PT.BALSRI/Administrasi/proses/edit_pengiriman_L8.php
@@ -43,7 +43,7 @@ else{
 }
 
 
-$u_dex = $dexlite*13350;
+$u_dex = $dexlite*13700;
 $uang_makan = 625*$jt_gps;
 $uang_jalan = $u_dex + $uang_makan;
 
--- PT.BALSRI/Administrasi/proses/proses_pengirimanP.php
+++ PT.BALSRI/Administrasi/proses/proses_pengirimanP.php
@@ -41,7 +41,7 @@ if($jen_ken == '8000 L'){
 else{
 	$dexlite = $jt_gps/6;
 }
-$u_dex = $dexlite*13350;
+$u_dex = $dexlite*13700;
 $uang_makan = 625*$jt_gps;
 $uang_jalan = $u_dex + $uang_makan;
 
