c542738c18f9ea4d282cc4f60be3b27f6568349b
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2025-01-31 16:23:25 +0700

    7 Januari 2024

--- PT.CBM/StaffAdmin/proses/edit_rekap_gaji_driver_pbr.php
+++ PT.CBM/StaffAdmin/proses/edit_rekap_gaji_driver_pbr.php
@@ -35,8 +35,8 @@ $bpjs_kesehatan = $_POST['bpjs_kesehatan'];
 $bpjs_ketenagakerjaan = $_POST['bpjs_ketenagakerjaan'];
 $uang_makan = $_POST['uang_makan'];
 $angsuran_bon_bulanan = $_POST['angsuran_bon_bulanan'];
-$total_gaji = $upah_nje + $uang_makan  + $upah_gas_palembang;
-$total_gaji_diterima =  $upah_nje + $uang_makan  + $upah_gas_palembang - $bpjs_ketenagakerjaan;
+$total_gaji = $upah_nje + $uang_makan  + $upah_nje2 + $upah_gas_palembang;
+$total_gaji_diterima =  $upah_nje + $uang_makan  + $upah_gas_palembang + $upah_nje2 - $bpjs_ketenagakerjaan;
 $keterangan = $_POST['keterangan'];
 $hutang_pribadi = $_POST['hutang_pribadi'];
 
