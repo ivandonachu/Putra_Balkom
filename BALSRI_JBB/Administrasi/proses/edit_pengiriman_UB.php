081ecb5bd4297a234176d40c851f9526e6454885
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2025-11-05 07:11:17 +0700

    27 OKTOBER 2025

--- BALSRI_JBB/Administrasi/proses/edit_pengiriman_UB.php
+++ BALSRI_JBB/Administrasi/proses/edit_pengiriman_UB.php
@@ -44,7 +44,7 @@ else{
 	$solar = $jt_gps/6;
 }
 
-$u_dex = $dexlite*14000;
+$u_dex = $dexlite*14200;
 $u_solar = $solar*12000;
 $uang_makan = 625*$jt_gps;
 $uang_jalan = $u_dex + $uang_makan;
--- BALSRI_JBB/Administrasi/proses/proses_pengiriman_TG.php
+++ BALSRI_JBB/Administrasi/proses/proses_pengiriman_TG.php
@@ -47,7 +47,7 @@ else{
 	$solar = $jt_gps/6;
 }
 
-$u_dex = $dexlite*13700;
+$u_dex = $dexlite*14200;
 $u_solar = $solar*12000;
 $uang_makan = 625*$jt_gps;
 $uang_jalan = $u_dex + $uang_makan;
