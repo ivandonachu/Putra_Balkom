c0b2b329172cdc57fd1e6bbe31ac1b67b8753b92
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2025-12-25 10:23:55 +0700

    3 Desember 2025

--- PT.CBM/Oprasional/view/VPengeluaranMesuji.php
+++ PT.CBM/Oprasional/view/VPengeluaranMesuji.php
@@ -268,7 +268,7 @@ Menu Kepala Oprasional
   <div class="pinggir1" style="margin-right: 20px; margin-left: 20px;">
 
 
-    <?php  echo "<form  method='POST' action='VPengeluaranSeberuk' style='margin-bottom: 15px;'>" ?>
+    <?php  echo "<form  method='POST' action='VPengeluaranMesuji' style='margin-bottom: 15px;'>" ?>
     <div>
       <div align="left" style="margin-left: 20px;"> 
         <input type="date" id="tanggal1" style="font-size: 14px" name="tanggal1"> 
