8c62f48429f8ac2c73a52a7755c933c465fedd43
DESKTOP-DIHSB0N\ASUS <ivandonachu13@gmail.com>
2026-07-05 15:42:46 +0700

    4 juli 2026

--- DirekturUtama/view/PERTASHOP/view/VLabaRugiPs.php
+++ DirekturUtama/view/PERTASHOP/view/VLabaRugiPs.php
@@ -239,7 +239,7 @@ else{
 
    //Sisa stok Akhir pertamax
 
-   $tabley1 = mysqli_query($koneksiperta, "SELECT MAX(no_penjualan) AS penjualan_akhir FROM penjualan a INNER JOIN pertashop b ON b.kode_perta=a.kode_perta WHERE tanggal BETWEEN '$tanggal_awal' AND '$tanggal_akhir' AND nama_barang = 'Pertamax' AND b.lokasi = '$lokasi' ");
+   $tabley1 = mysqli_query($koneksiperta, "SELECT MAX(no_penjualan) AS penjualan_akhir FROM penjualan a INNER JOIN pertashop b ON b.kode_perta=a.kode_perta WHERE tanggal BETWEEN '$tanggal_awal' AND '$tanggal_akhir' AND nama_barang = 'Pertamax' AND b.lokasi = '$lokasi' ORDER BY tanggal ");
 
    $datay1 = mysqli_fetch_array($tabley1);
    $no_penjualan_akhir_max = $datay1['penjualan_akhir'];
